e10c40981ac8ea527c4df5bfbe578efc9a4f6dc8
CASIMIRO CAMARA CANTO <canto1951@gmail.com>
2025-04-09 11:52:23 -0300

    Add files via upload

---
+++ Exercicio 5.6r.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a;
+    
+    for(a = 0; a <= 1; a++) {
+        printf("A: %d  ! %d = %d\n", a, a, !a);
+    }
+    
+    return 0;
+}
