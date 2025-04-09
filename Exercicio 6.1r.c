66fb560d9959007a86f520087e0bc466f7262090
CASIMIRO CAMARA CANTO <canto1951@gmail.com>
2025-04-09 11:51:04 -0300

    Add files via upload

---
+++ Exercicio 6.1r.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+// Definição da função soma
+int soma(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int resultado = soma(5, 3);
+    printf("5 + 3 = %d\n", resultado);
+    
+    return 0;
+}
